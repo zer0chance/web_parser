e1a1d90753fb9cbc919b642d9d0e0a9c991f2e08
zer0chance <ier7928@gmail.com>
2020-03-09 16:19:22 +0700

    Correct typing mistake

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     for i in range(25):
         name_and_price = rows[i].find_all("a")      # find all links, because name and price is links
         market_cup = rows[i].find_all(class_ = "")[1].get_text()
-        currencies[name_and_price[0].get_text()] = [name_and_price[1].get_text(), market_cup]   # name - key
+        currencies[name_and_price[0].get_text()] = [name_and_price[1].get_text(), market_cup]   # key - name 
                                                                                                 # value - list of [price, market_cup]    
     while(True):
         print("Type cryptocurrency`s name or \"exit\":", end=" ")
@@ -23,4 +23,4 @@ if __name__ == "__main__":
             print("\n Price:      " + currencies[name][0])
             print(" Market cap: " + currencies[name][1], end="\n\n")
         else:
-            print("\n No such cryptocurrenscy in top 25 \n")    
+            print("\n No such cryptocurrency in top 25 \n")    
